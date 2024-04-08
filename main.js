370ce1897b341e0ff89ddef1f2442a0ce0b7eb81
Matheus Matos <mf08727@outlook.com>
2024-04-08 17:12:34 -0300

    Remove Pool de Conexão do SQL Server e Biblioteca MSSQL

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const serialport = require('serialport');
 const express = require('express');
 const mysql = require('mysql2');
-const sql = require('mssql');
 
 // não altere!
 const SERIAL_BAUD_RATE = 9600;
@@ -13,11 +12,6 @@ const SERVIDOR_PORTA = 3300;
 // true -> insere
 const HABILITAR_OPERACAO_INSERIR = false;
 
-// altere o valor da variável AMBIENTE para o valor desejado:
-// API conectada ao banco de dados remoto, SQL Server -> 'producao'
-// API conectada ao banco de dados local, MySQL Workbench - 'desenvolvimento'
-const AMBIENTE = 'desenvolvimento';
-
 const serial = async (
     valoresDht11Umidade,
     valoresDht11Temperatura,
@@ -27,23 +21,16 @@ const serial = async (
 ) => {
     let poolBancoDados = ''
 
-    if (AMBIENTE == 'desenvolvimento') {
-        poolBancoDados = mysql.createPool(
-            {
-                // altere!
-                // CREDENCIAIS DO BANCO - MYSQL WORKBENCH
-                host: 'localhost',
-                user: 'USUARIO_DO_BANCO_LOCAL',
-                password: 'SENHA_DO_BANCO_LOCAL',
-                database: 'DATABASE_LOCAL'
-            }
-        ).promise();
-    } else if (AMBIENTE == 'producao') {
-        console.log('Projeto rodando inserindo dados em nuvem. Configure as credenciais abaixo.');
-    } else {
-        throw new Error('Ambiente não configurado. Verifique o arquivo "main.js" e tente novamente.');
-    }
-
+    poolBancoDados = mysql.createPool(
+        {
+            // altere!
+            // CREDENCIAIS DO BANCO - MYSQL WORKBENCH
+            host: 'localhost',
+            user: 'USUARIO_DO_BANCO_LOCAL',
+            password: 'SENHA_DO_BANCO_LOCAL',
+            database: 'DATABASE_LOCAL'
+        }
+    ).promise();
 
     const portas = await serialport.SerialPort.list();
     const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
@@ -75,45 +62,20 @@ const serial = async (
         valoresChave.push(chave);
 
         if (HABILITAR_OPERACAO_INSERIR) {
-            if (AMBIENTE == 'producao') {
-                // altere!
-                // Este insert irá inserir os dados na tabela "medida"
-                // -> altere nome da tabela e colunas se necessário
-                // Este insert irá inserir dados de fk_aquario id=1 (fixo no comando do insert abaixo)
-                // >> Importante! você deve ter o aquario de id 1 cadastrado.
-                sqlquery = `INSERT INTO medida (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave, momento, fk_aquario) VALUES (${dht11Umidade}, ${dht11Temperatura}, ${luminosidade}, ${lm35Temperatura}, ${chave}, CURRENT_TIMESTAMP, 1)`;
 
-                // CREDENCIAIS DO BANCO REMOTO - SQL SERVER
-                // Importante! você deve ter criado o usuário abaixo com os comandos presentes no arquivo
-                // "script-criacao-usuario-sqlserver.sql", presente neste diretório.
-                const connStr = "Server=servidor-acquatec.database.windows.net;Database=bd-acquatec;User Id=usuarioParaAPIArduino_datawriter;Password=#Gf_senhaParaAPI;";
-
-                function inserirComando(conn, sqlquery) {
-                    conn.query(sqlquery);
-                    console.log("valores inseridos no banco: ", dht11Umidade + ", " + dht11Temperatura + ", " + luminosidade + ", " + lm35Temperatura + ", " + chave)
-                }
-
-                sql.connect(connStr)
-                    .then(conn => inserirComando(conn, sqlquery))
-                    .catch(err => console.log("erro! " + err));
-
-            } else if (AMBIENTE == 'desenvolvimento') {
-
-                // altere!
-                // Este insert irá inserir os dados na tabela "medida"
-                // -> altere nome da tabela e colunas se necessário
-                // Este insert irá inserir dados de fk_aquario id=1 (fixo no comando do insert abaixo)
-                // >> você deve ter o aquario de id 1 cadastrado.
-                await poolBancoDados.execute(
-                    'INSERT INTO medida (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave, momento, fk_aquario) VALUES (?, ?, ?, ?, ?, now(), 1)',
-                    [dht11Umidade, dht11Temperatura, luminosidade, lm35Temperatura, chave]
-                );
-                console.log("valores inseridos no banco: ", dht11Umidade + ", " + dht11Temperatura + ", " + luminosidade + ", " + lm35Temperatura + ", " + chave)
-
-            } else {
-                throw new Error('Ambiente não configurado. Verifique o arquivo "main.js" e tente novamente.');
-            }
+            // altere!
+            // Este insert irá inserir os dados na tabela "medida"
+            // -> altere nome da tabela e colunas se necessário
+            // Este insert irá inserir dados de fk_aquario id=1 (fixo no comando do insert abaixo)
+            // >> você deve ter o aquario de id 1 cadastrado.
+            await poolBancoDados.execute(
+                'INSERT INTO medida (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave) VALUES (?, ?, ?, ?, ?)',
+                [dht11Umidade, dht11Temperatura, luminosidade, lm35Temperatura, chave]
+            );
+            console.log("valores inseridos no banco: ", dht11Umidade + ", " + dht11Temperatura + ", " + luminosidade + ", " + lm35Temperatura + ", " + chave)
+        
         }
+        
     });
     arduino.on('error', (mensagem) => {
         console.error(`Erro no arduino (Mensagem: ${mensagem}`)
