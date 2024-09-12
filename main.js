639ed55d7b02531acf51ccf2f1cadabd2a2b641b
Matheus Matos <mf08727@outlook.com>
2024-09-12 11:08:28 -0300

    Ajuste - Script coletar dois valores, analogico e digital

--- main.js
+++ main.js
@@ -1,50 +1,40 @@
-// Importa os módulos necessários
-// não altere!
-const serialport = require('serialport'); // Módulo para comunicação serial
-const express = require('express'); // Módulo para criar um servidor web
-const mysql = require('mysql2'); // Módulo para conectar ao MySQL
+// importa os bibliotecas necessários
+const serialport = require('serialport');
+const express = require('express');
+const mysql = require('mysql2');
 
-// Constantes para configurações
-// não altere!
+// constantes para configurações
 const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
-// Habilita ou desabilita a inserção de dados no banco de dados
-// false -> nao insere
-// true -> insere
+// habilita ou desabilita a inserção de dados no banco de dados
 const HABILITAR_OPERACAO_INSERIR = false;
 
-// Função para comunicação serial
+// função para comunicação serial
 const serial = async (
-    valoresDht11Umidade,
-    valoresDht11Temperatura,
-    valoresLuminosidade,
-    valoresLm35Temperatura,
-    valoresChave
+    valoresSensorAnalogico,
+    valoresSensorDigital,
 ) => {
-    let poolBancoDados = ''
 
-    // Conexão com o banco de dados MySQL
-    poolBancoDados = mysql.createPool(
+    // conexão com o banco de dados MySQL
+    let poolBancoDados = mysql.createPool(
         {
-            // altere!
-            // Credenciais do banco de dados
-            host: 'localhost',
-            user: 'USUARIO_DO_BANCO_LOCAL',
-            password: 'SENHA_DO_BANCO_LOCAL',
-            database: 'DATABASE_LOCAL',
+            host: 'HOST_DO_BANCO',
+            user: 'USUARIO_DO_BANCO',
+            password: 'SENHA_DO_BANCO',
+            database: 'DATABASE_DO_BANCO',
             port: 3306
         }
     ).promise();
 
-    // Lista as portas seriais disponíveis e procura pelo Arduino
+    // lista as portas seriais disponíveis e procura pelo Arduino
     const portas = await serialport.SerialPort.list();
     const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
     if (!portaArduino) {
         throw new Error('O arduino não foi encontrado em nenhuma porta serial');
     }
 
-    // Configura a porta serial com o baud rate especificado
+    // configura a porta serial com o baud rate especificado
     const arduino = new serialport.SerialPort(
         {
             path: portaArduino.path,
@@ -52,115 +42,85 @@ const serial = async (
         }
     );
 
-    // Evento quando a porta serial é aberta
+    // evento quando a porta serial é aberta
     arduino.on('open', () => {
         console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
     });
 
-    // Processa os dados recebidos do Arduino
+    // processa os dados recebidos do Arduino
     arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
         console.log(data);
         const valores = data.split(';');
-        const dht11Umidade = parseFloat(valores[0]);
-        const dht11Temperatura = parseFloat(valores[1]);
-        const lm35Temperatura = parseFloat(valores[2]);
-        const luminosidade = parseFloat(valores[3]);
-        const chave = parseInt(valores[4]);
+        const sensorDigital = parseInt(valores[0]);
+        const sensorAnalogico = parseFloat(valores[1]);
 
-        // Armazena os valores dos sensores nos arrays correspondentes
-        valoresDht11Umidade.push(dht11Umidade);
-        valoresDht11Temperatura.push(dht11Temperatura);
-        valoresLuminosidade.push(luminosidade);
-        valoresLm35Temperatura.push(lm35Temperatura);
-        valoresChave.push(chave);
+        // armazena os valores dos sensores nos arrays correspondentes
+        valoresSensorAnalogico.push(sensorAnalogico);
+        valoresSensorDigital.push(sensorDigital);
 
-        // Insere os dados no banco de dados (se habilitado)
+        // insere os dados no banco de dados (se habilitado)
         if (HABILITAR_OPERACAO_INSERIR) {
 
-            // altere!
-            // Este insert irá inserir os dados na tabela "medida"
+            // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave) VALUES (?, ?, ?, ?, ?)',
-                [dht11Umidade, dht11Temperatura, luminosidade, lm35Temperatura, chave]
+                'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
+                [sensorAnalogico, sensorDigital]
             );
-            console.log("valores inseridos no banco: ", dht11Umidade + ", " + dht11Temperatura + ", " + luminosidade + ", " + lm35Temperatura + ", " + chave)
-        
+            console.log("valores inseridos no banco: ", sensorAnalogico + ", " + sensorDigital);
+
         }
-        
+
     });
 
-    // Evento para lidar com erros na comunicação serial
+    // evento para lidar com erros na comunicação serial
     arduino.on('error', (mensagem) => {
         console.error(`Erro no arduino (Mensagem: ${mensagem}`)
     });
 }
 
-
-// não altere!
-// Função para criar e configurar o servidor web
+// função para criar e configurar o servidor web
 const servidor = (
-    valoresDht11Umidade,
-    valoresDht11Temperatura,
-    valoresLuminosidade,
-    valoresLm35Temperatura,
-    valoresChave
+    valoresSensorAnalogico,
+    valoresSensorDigital
 ) => {
     const app = express();
 
-    // Configurações de CORS
+    // configurações de requisição e resposta
     app.use((request, response, next) => {
         response.header('Access-Control-Allow-Origin', '*');
         response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
         next();
     });
 
-    // Inicia o servidor na porta especificada
+    // inicia o servidor na porta especificada
     app.listen(SERVIDOR_PORTA, () => {
         console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
     });
 
-    // Define os endpoints da API para cada tipo de sensor
-    app.get('/sensores/dht11/umidade', (_, response) => {
-        return response.json(valoresDht11Umidade);
+    // define os endpoints da API para cada tipo de sensor
+    app.get('/sensores/analogico', (_, response) => {
+        return response.json(valoresSensorAnalogico);
     });
-    app.get('/sensores/dht11/temperatura', (_, response) => {
-        return response.json(valoresDht11Temperatura);
-    });
-    app.get('/sensores/luminosidade', (_, response) => {
-        return response.json(valoresLuminosidade);
-    });
-    app.get('/sensores/lm35/temperatura', (_, response) => {
-        return response.json(valoresLm35Temperatura);
-    });
-    app.get('/sensores/chave', (_, response) => {
-        return response.json(valoresChave);
+    app.get('/sensores/digital', (_, response) => {
+        return response.json(valoresSensorDigital);
     });
 }
 
-// Função principal assíncrona para iniciar a comunicação serial e o servidor web
+// função principal assíncrona para iniciar a comunicação serial e o servidor web
 (async () => {
-    // Arrays para armazenar os valores dos sensores
-    const valoresDht11Umidade = [];
-    const valoresDht11Temperatura = [];
-    const valoresLuminosidade = [];
-    const valoresLm35Temperatura = [];
-    const valoresChave = [];
+    // arrays para armazenar os valores dos sensores
+    const valoresSensorAnalogico = [];
+    const valoresSensorDigital = [];
 
-    // Inicia a comunicação serial
+    // inicia a comunicação serial
     await serial(
-        valoresDht11Umidade,
-        valoresDht11Temperatura,
-        valoresLuminosidade,
-        valoresLm35Temperatura,
-        valoresChave
+        valoresSensorAnalogico,
+        valoresSensorDigital
     );
 
-    // Inicia o servidor web
+    // inicia o servidor web
     servidor(
-        valoresDht11Umidade,
-        valoresDht11Temperatura,
-        valoresLuminosidade,
-        valoresLm35Temperatura,
-        valoresChave
+        valoresSensorAnalogico,
+        valoresSensorDigital
     );
 })();
